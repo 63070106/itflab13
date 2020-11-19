a16c6a6be82ab52866ef16cb96676b5d632c0c09
63070106 <73683220+63070106@users.noreply.github.com>
2020-11-19 21:28:38 +0700

    Update show.php

--- show.php
+++ show.php
@@ -1,8 +1,14 @@
 <html>
 <head>
-<title>ITF Lab</title>
+    <title>ITF Lab</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
-<body>
+    <body>
 <?php
 $conn = mysqli_init();
 mysqli_real_connect($conn, 'lab-13.mysql.database.azure.com', 'Pannathorn@lab-13', 'OIOuiv83', 'itflab', 3306);
@@ -12,27 +18,38 @@ if (mysqli_connect_errno($conn))
 }
 $res = mysqli_query($conn, 'SELECT * FROM guestbook');
 ?>
-<table width="600" border="1">
-  <tr>
-    <th width="100"> <div align="center">Name</div></th>
-    <th width="350"> <div align="center">Comment </div></th>
-    <th width="150"> <div align="center">Link </div></th>
-  </tr>
+<br>
+<div class="container">
+    <table class = "table table-bordered table-hover" width="1200" align="center" border="1">
+        <thead class="thead-dark">
+    <tr>
+        <th width="300"> <div align="center">Name</div></th>
+        <th width="300"> <div align="center">Comment </div></th>
+        <th width="300"> <div align="center">Link </div></th>
+        <th width="300"> <div align="center">ID</div></th>
+    </tr>
+    </thead>
 <?php
 while($Result = mysqli_fetch_array($res))
 {
 ?>
-  <tr>
-    <td><?php echo $Result['Name'];?></div></td>
-    <td><?php echo $Result['Comment'];?></td>
-    <td><?php echo $Result['Link'];?></td>
-  </tr>
+        <tbody>
+                <tr>
+            <td><?php echo $Result['Name'];?></td>
+            <td><?php echo $Result['Comment'];?></td>
+            <td><?php echo $Result['Link'];?></td>
+            <td>
+                <a href="edit.php?ID=<?php echo $Result['ID']?>" class="btn btn-outline-success" >EDIT</a>
+                <a href="del.php?ID=<?php echo $Result['ID']?>" class="btn btn-outline-danger"onclick="return confirm('Confirm data deletion?')">DELETE</a>
+                </tr>
+        </tbody>
 <?php
 }
 ?>
-</table>
+    </table>
+    <button type="button" class="btn btn-outline-warning" onclick ="window.location.href='form.html'">ADD</button> 
 <?php
 mysqli_close($conn);
 ?>
-</body>
+    </body>
 </html>
